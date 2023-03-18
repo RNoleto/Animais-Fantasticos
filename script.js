5c84a8207d112c940fd1e46a492cf86031d9b760
Bismarck Bandeira <ramon.zap@hotmail.com>
2023-03-18 13:32:51 -0300

    criação de function de navegação com Accordion List

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 // Navegação por tabs
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-
-if(tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add('ativo');
-  //Inserir e remover ativo as sections
-  function activeTab(index){
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
+function initTabNav(){
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+  
+  if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+    //Inserir e remover ativo as sections
+    function activeTab(index){
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
+    }
+    //Inserir e remover ativo às sections selecionadas de acordo com a imagem clicada
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add('ativo');
   }
-  //Inserir e remover ativo às sections selecionadas de acordo com a imagem clicada
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    });
-  });
 }
+initTabNav();
+// Navegação Accordion List na Section FAQ
+function initAccordion(){
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if(accordionList.length){
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+    
+    
+    function activeAccordion(){
+      this.classList.toggle('activeClass');
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    })
+  }
+}
+initAccordion();
