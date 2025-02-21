c447871adaf3c8af363a6d5adb025c63644317ec
Ramon Noleto <devnoleto@gmail.com>
2025-02-21 16:55:42 -0300

    :tada: feat: Criação do fetch-biticoins usando api externa e melhoria do fetch-animais usando tryCatch

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBiticoin from "./modules/fetch-biticoins.js";
 
 
 initScrollSuave();
@@ -19,6 +19,6 @@ initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
-
 initFuncionamento();
 initFetchAnimais();
+initFetchBiticoin();
