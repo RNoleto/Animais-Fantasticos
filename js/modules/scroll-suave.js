c098df67d4aeba7f32aed147f428ecf00bdfec43
Ramon Noleto <devnoleto@gmail.com>
2025-02-28 10:40:59 -0300

    ScrollSuave transformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,26 +1,32 @@
-export default function initScrollSuave(){
-    //Scroll Suave Link Interno
-    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]'); // Pegar os links internos da página a[href^="#"]
-    
-    function scrollToSection(event){
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute('href');
-      const section = document.querySelector(href);
-      // forma mais simples de scroll suave interno
-      section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    
-     //forma alternativa de scroll suave interno
-      // const topo = section.offsetTop;
-      // window.scroll({
-      //   top: topo,
-      //   behavior: 'smooth',
-      // });
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
     }
-    
-    linksInternos.forEach((link) => {
-      link.addEventListener('click', scrollToSection)
-    })
+
+    this.scrollToSection = this.scrollToSection.bind(this);
   }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
+}
