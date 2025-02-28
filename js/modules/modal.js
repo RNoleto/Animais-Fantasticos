912eaf0a12303630c1d06f716651e5d9b3d0b5a5
Ramon Noleto <devnoleto@gmail.com>
2025-02-28 15:56:10 -0300

    modal refatorado com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,25 +1,38 @@
-export default function initModal() {
+export default class Modal {
+    constructor(botaoAbrir, botaoFechar, containerModal) {
+        this.botaoAbrir = document.querySelector(botaoAbrir);
+        this.botaoFechar = document.querySelector(botaoFechar);
+        this.containerModal = document.querySelector(containerModal);
 
-    const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-    const botaoFechar = document.querySelector('[data-modal="fechar"]');
-    const containerModal = document.querySelector('[data-modal="container"]');
+        this.eventToggleModal = this.eventToggleModal.bind(this);
+        this.cliqueForaModal = this.cliqueForaModal.bind(this);
+    }
 
-    if (botaoAbrir && botaoAbrir && containerModal) {
+    toggleModal() {
+        this.containerModal.classList.toggle('ativo');
+    }
 
-        function toggleModal(event) {
-            event.preventDefault();
-            containerModal.classList.toggle('ativo');
+    eventToggleModal(event) {
+        event.preventDefault();
+        this.toggleModal();
+    }
+
+    cliqueForaModal(event) {
+        if (event.target === this.containerModal) {
+            this.toggleModal();
         }
+    }
 
-        function cliqueForaModal(event) {
-            if (event.target === this) {
-                toggleModal(event);
-            }
+    addModalEvents() {
+        this.botaoAbrir.addEventListener('click', this.eventToggleModal);
+        this.botaoFechar.addEventListener('click', this.eventToggleModal);
+        this.containerModal.addEventListener('click', this.cliqueForaModal);
+    }
+
+    init() {
+        if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+            this.addModalEvents();
         }
-
-        botaoAbrir.addEventListener('click', toggleModal);
-        botaoFechar.addEventListener('click', toggleModal);
-        containerModal.addEventListener('click', cliqueForaModal);
+        return this;
     }
 }
-
